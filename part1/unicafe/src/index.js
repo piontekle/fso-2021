28a27027c02964df44e19b3f90f43fea546348df
piontekle <piontekle@gmail.com>
2021-01-12 10:50:59 -0500

    Exercises 1.7-1.9 Statistics refactored

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,16 +9,60 @@ const Button = ({ handleClick, rating }) => (
 
 const Display = ({ rating, value }) => <div>{rating} {value}</div>
 
+const Statistics = ({ good, neutral, bad, total, avg, avgGood }) => {
+  if (total === 0) return <div>No feedback given</div>
+
+  return (
+    <div>
+      <Display rating="good" value={good} />
+      <Display rating="neutral" value={neutral} />
+      <Display rating="bad" value={bad} />
+      <Display rating="total" value={total} />
+      <Display rating="average" value={avg} />
+      <Display rating="average positive" value={avgGood} />
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [avgGood, setAvgGood] = useState(0);
 
-  const handleClick = (rating, value) => {
-    if (rating === "good") setGood(good + 1);
-    else if (rating === "neutral") setNeutral(neutral + 1);
-    else setBad(bad + 1);
+  const handleClick = (rating) => {
+    let goodCt = good;
+    let neutralCt = neutral;
+    let badCt = bad;
+    const totalCt = total + 1;
+
+    setTotal(totalCt);
+
+    if (rating === "good") {
+      goodCt += 1;
+      setGood(goodCt);
+    } else if (rating === "neutral") {
+      neutralCt += 1;
+      setNeutral(neutralCt);
+    } else {
+      badCt += 1;
+      setBad(badCt)
+    };
+
+    setAverages(goodCt, badCt, totalCt);
+  }
+
+  const setAverages = (good, bad, total) => {
+    let totalScore = good - bad;
+
+    const average = totalScore !== 0 ? totalScore / (total) : 0;
+    const avgGood = good > 0 ? (good / (total)) * 100 : 0;
+
+    setAverage(average);
+    setAvgGood(avgGood);
   }
 
   return (
@@ -28,9 +72,14 @@ const App = () => {
       <Button handleClick={handleClick} rating="neutral" />
       <Button handleClick={handleClick} rating="bad" />
       <Header title="statistics" />
-      <Display rating="good" value={good} />
-      <Display rating="neutral" value={neutral} />
-      <Display rating="bad" value={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        avg={average}
+        avgGood={avgGood}
+      />
     </div>
   )
 }
