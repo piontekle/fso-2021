49cad011c9397b04b1a2c0e8c6f770a7da9691e9
piontekle <piontekle@gmail.com>
2021-02-10 08:44:16 -0500

    Use existing phonebook app

--- part3/pb_frontend/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = ({ label, type, onClick }) => (
-  <button type={type} onClick={onClick}>{label}</button>
-)
-
-export default Button;
--- part3/pb_frontend/src/components/TextBox.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TextBox = ({ onChange, value }) => (
-  <input onChange={onChange} value={value} />
-)
-
-export default TextBox;
