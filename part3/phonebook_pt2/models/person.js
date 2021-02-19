fafb4e1c15ca7c1218f142953ecdfb48c49651f4
piontekle <piontekle@gmail.com>
2021-02-19 09:24:39 -0500

    uibuild

--- part3/phonebook_pt2/models/person.js
+++ part3/phonebook_pt2/models/person.js
@@ -10,8 +10,14 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
