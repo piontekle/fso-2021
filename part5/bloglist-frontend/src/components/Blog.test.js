453444f62bf65e7c3ebea93d6ae4507a3cb4768c
piontekle <piontekle@gmail.com>
2021-04-01 09:37:04 -0400

    Ex 5.14: view button tests

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders only title and author by default', () => {
-  const blog = {
-    id: 123,
-    title: 'Testing in a Blog',
-    author: 'Bloggy McBlogger',
-    likes: 23,
-    url: 'https://blogsrus.com',
-    user: { name: 'Oliv Blogs' }
-  }
+describe('<Blog />', () => {
+  let component
 
-  const onUpdateBlog = jest.fn()
-  const onRemoveBlog = jest.fn()
+  beforeEach(() => {
+    const blog = {
+      id: 123,
+      title: 'Testing in a Blog',
+      author: 'Bloggy McBlogger',
+      likes: 23,
+      url: 'https://blogsrus.com',
+      user: { name: 'Oliv Blogs' }
+    }
+    const onUpdateBlog = jest.fn()
+    const onRemoveBlog = jest.fn()
 
-  const component = render(
-    <Blog blog={blog} onUpdateBlog={onUpdateBlog} onRemoveBlog={onRemoveBlog} />
-  )
+    component = render(
+      <Blog blog={blog} onUpdateBlog={onUpdateBlog} onRemoveBlog={onRemoveBlog} />
+    )
+  })
 
-  const div = component.container.querySelector("[data-testid='blog-123']")
-  expect(div).toHaveTextContent('Testing in a Blog')
-  expect(div).toHaveTextContent('Bloggy McBlogger')
-  expect(div).not.toHaveTextContent('https://blogsrus.com')
+  test('renders only title and author by default', () => {
+    const div = component.container.querySelector("[data-testid='blog-123']")
+    expect(div).toHaveTextContent('Testing in a Blog')
+    expect(div).toHaveTextContent('Bloggy McBlogger')
+    expect(div).not.toHaveTextContent('https://blogsrus.com')
+    expect(div).not.toHaveTextContent('Oliv Blogs')
+    expect(div).not.toHaveTextContent(23)
+  })
+
+  test('renders blog details when view is clicked', () => {
+    const viewButton = component.getByText('view')
+    fireEvent.click(viewButton)
+
+    const div = component.container.querySelector("[data-testid='blog-123']")
+    expect(div).toHaveTextContent('Testing in a Blog')
+    expect(div).toHaveTextContent('Bloggy McBlogger')
+    expect(div).toHaveTextContent('https://blogsrus.com')
+    expect(div).toHaveTextContent('Oliv Blogs')
+    expect(div).toHaveTextContent(23)
+  })
 })
